18b064da11bc90263d78a92b2dcee3c378481bc2
ahlersml <70450921+ahlersml@users.noreply.github.com>
2021-04-10 13:44:26 -0400

    1 Enemy added and it patrols an area

---
+++ Assets/Scripts/Enemy1_Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1_Controller : MonoBehaviour
+{
+
+    public bool MoveRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 position = transform.position;
+
+        if(position.x < 1)
+        {
+            MoveRight = true;
+        }
+
+        if(position.x > 4)
+        {
+            MoveRight = false;
+        }
+
+
+
+        //move left or right based on MoveRight variable
+        if (MoveRight == true)
+        {
+            transform.Translate(1 * Time.deltaTime, 0, 0);
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            transform.Translate(-1 * Time.deltaTime, 0, 0);
+            transform.localScale = new Vector2(1, 1);
+        }
+
+    }
+}
